e23dce414d942505dddcb1b824c62330a31d9cf5
pcgp <114573948+pcgp@users.noreply.github.com>
2023-01-18 12:35:07 +0100

    Added user interaction for the API request

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,11 +30,13 @@ streamlit.dataframe(fruits_to_show)
 
 #New section to display fruityvice API response
 streamlit.header("Fruityvice Fruit Advice!")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi') #Allow the user select a fruit
+streamlit.write('The user entered ', fruit_choice)
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
-
+#request the data to the API
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+# streamlit.text(fruityvice_response)
 # get the response json data and normalizes it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # show the normalized json as a table
