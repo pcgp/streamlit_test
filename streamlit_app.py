ec720bbab943a13a4fe9721160be05252d115200
pcgp <114573948+pcgp@users.noreply.github.com>
2023-01-20 12:52:48 +0100

    connection closing calls

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,7 +47,7 @@ try:
 except URLError as e:
   streamlit.error()
     
-streamlit.header("The fruit load list contains:")
+streamlit.header("View Our Fruit List - Add Your Favorites!")
 def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
     my_cur.execute("select fruit_name from fruit_load_list")
@@ -57,6 +57,7 @@ def get_fruit_load_list():
 if streamlit.button('Get Fruit Load List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   my_data_rows = get_fruit_load_list()
+  my_cnx.close()
   streamlit.dataframe(my_data_rows)
 
 # Allowing the user to add a fruit to the list
@@ -69,4 +70,5 @@ add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 if streamlit.button('Add a Fruit to the list'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   back_from_function = insert_row_snowflake(add_my_fruit)
+  my_cnx.close()
   streamlit.text(back_from_function)
